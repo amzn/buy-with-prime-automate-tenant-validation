65a539e7fad808614039f40a9a96afb45bd8c6d2
Jiwon Yeom <jiwony@amazon.com>
2023-09-08 21:32:32 -0700

    log added

--- ddbApi.js
+++ ddbApi.js
@@ -31,6 +31,9 @@ async function createDDBItem(
     },
   };
   const command = new DDB.PutItemCommand(input);
+  console.log(command)
+
+  
   try {
     const response = await ddbClient.send(command);
     return response.$metadata.httpStatusCode;
